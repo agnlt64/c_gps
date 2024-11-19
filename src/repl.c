8b02f36b84e322a4ee84d5ad4404d341cfcb0784
Antonin GENELOT <jaimelesfraisesbleues@gmail.com>
2024-11-19 10:05:13 +0100

    code cleaning

--- src/repl.c
+++ src/repl.c
@@ -13,16 +13,6 @@ static void print_row_border(int n)
     printf("+\n");
 }
 
-int my_strlen(const char* str)
-{
-    int i = 0;
-    while (str[i] != '\0')
-    {
-        i++;
-    }
-    return i;
-}
-
 void repl_help()
 {
 
